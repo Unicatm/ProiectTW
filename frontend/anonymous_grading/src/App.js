f315b4dd9bdca65ec2770d55968777397040ccd1
Unicatm <alexandra.tertiscu@gmail.com>
2024-12-14 14:37:31 +0200

    foldere fe

--- frontend/anonymous_grading/src/App.js
+++ frontend/anonymous_grading/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Tehnologii Web
+          Tehnologii Webbbbb
         </a>
       </header>
     </div>
